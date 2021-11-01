fb7e64282cb41f17794fa51bef789cdaaff73265
anuragyadav20062002 <anuragyadav20062002@gmail.com>
2021-11-01 23:41:11 +0530

    created mongoose model

--- app.js
+++ app.js
@@ -10,6 +10,16 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(express.static("public"))
 
+mongoose.connect("mongod://localhost:27017/wikiDB", { userNewParser: true })
+
+const articleschema = {
+  title: String,
+  content: String,
+}
+
+const Article = mongoose.model("Article", articleschema)
+
+///////listening/////////
 app.listen(3000, function () {
   console.log("Server started successfully at port 3000")
 })
