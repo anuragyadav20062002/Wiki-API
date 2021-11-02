4b1778120e3e80cc0a00119bd73cd0b07e48a695
anuragyadav20062002 <anuragyadav20062002@gmail.com>
2021-11-02 19:06:28 +0530

    added post request by postman

--- app.js
+++ app.js
@@ -29,6 +29,19 @@ app.get("/articles", function (req, res) {
   })
 })
 
+app.post("/articles", function (req, res) {
+  const newArticle = new Article({
+    title: req.body.title,
+    content: req.body.content,
+  })
+
+  newArticle.save(function (err) {
+    if (!err) {
+      res.send("Successfully added new document in database")
+    }
+  })
+})
+
 ///////listening/////////
 app.listen(3000, function () {
   console.log("Server started successfully at port 3000")
